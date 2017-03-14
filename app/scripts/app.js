267f1e672ae2bfa7e8b333dac76d66a1c0226c90
Pentico <morulaneat@gmail.com>
2017-03-15 00:21:49 +0200

    first init

---
+++ app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name practicalAssignmentApp
+ * @description
+ * # practicalAssignmentApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('practicalAssignmentApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name practicalAssignmentApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the practicalAssignmentApp
+ */
+angular.module('practicalAssignmentApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
