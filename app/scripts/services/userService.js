022a629c12c04feb254e883e271b73187116e84e
Pentico <morulaneat@gmail.com>
2017-03-15 23:59:25 +0200

    skeleton of userService

--- app/scripts/services/userService.js
+++ app/scripts/services/userService.js
@@ -4,13 +4,80 @@
  * Defines the service that perfroms user authentication
  */
 angular.module('practicalAssignmentApp')
-.service('userService',[function() {
+.service('userService',['$http', function($http) {
+
+    var urlBase_user = '';
+    var Token = '';
+    var username = '';
+    var password = '';
 
     return {
 
-        test : function(){
-            return name;
+        getToken : function(){
+            return Token;
+        },
+
+        validateToken : function() {
+            if(Token != ''){
+                return true;
+            }else {
+                return false;
+            }
+        },
+
+        /**
+         * return a list of all the users projects
+         */
+        getProjects : function(){
+
+        },
+
+        /**
+         * Adds a new Project 
+         */
+        putProject : function(project) {
+
         },
         
+
+        /**
+         * deletes a project from server 
+         * param : pk of the project
+         */
+        deleteProject : function(key) {
+
+        },
+
+        /**
+         * Get a Specific project using pk
+         * param : pk of the project
+         */ 
+        getProject : function(key) {
+
+        },
+
+        /**
+         * Get a Specific task using pk
+         * param : pk of the project
+         */ 
+        getTask : function(key) {
+
+        },
+
+        /**
+         * deletes a Specific task using pk
+         * param : pk of the project
+         */ 
+        deleteTask : function(key) {
+
+        },
+
+        /**
+         * Adds a task to project
+         * param : task of the project
+         */
+        putTask : function(task) {
+
+        }
     };
 }]);
