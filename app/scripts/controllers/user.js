e5e911d11af7bd308ca15cdfe5e5ff7bdd2354c3
Pentico <morulaneat@gmail.com>
2017-03-16 11:12:47 +0200

    authenticate

--- app/scripts/controllers/user.js
+++ app/scripts/controllers/user.js
@@ -7,23 +7,25 @@ angular.module('practicalAssignmentApp')
     .controller('userController',['$scope', 'userService', function($scope, userService){
         $scope.username ='';
         $scope.password = '';
-        $scope.flashMessage = '';
+        $scope.flashMessage = '';        
 
         var authentication  = function() {
-            
-         userService.postLogin('jacob.zuma','tangent')
+
+         userService.postLogin(username, password)
                     .then (function(response){
                         $scope.name= response.data.token;
                         userService.setToken(response.data.token);
 
                         console.log(response);
                     }, function(error) {
-                        $scope.flashMessage = "Error Trying to Login in Please try again! "
+                        $scope.flashMessage = 'Error Trying to Login in Please try again! ';
                         console.log('on Error userController' + error);
                     });
          };
 
-         authentication();
         
-        
+        $scope.validate = function() {
+
+            console.log('on susdf');
+        };
 }]);
